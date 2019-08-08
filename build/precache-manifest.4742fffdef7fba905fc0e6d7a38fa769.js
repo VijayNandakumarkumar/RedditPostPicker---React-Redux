c3f672695944e88babe41b3858f75d8a2f432087
Vijaykumar N <vijay.nandakumar30@gmail.com>
2019-08-09 01:10:59 +0530

    Adding build

---
+++ build/precache-manifest.4742fffdef7fba905fc0e6d7a38fa769.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16d1473d638826ab0e6eac831b7e280a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3c841bd812592b30b384",
+    "url": "/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "19ab2e6a9794b30a169a",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "3c841bd812592b30b384",
+    "url": "/static/js/2.5942b638.chunk.js"
+  },
+  {
+    "revision": "19ab2e6a9794b30a169a",
+    "url": "/static/js/main.355348a1.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
